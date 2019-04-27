acebf42c0b99628edab5bb05bcc082d48b84afde
Jiyoung <jiyoung@macbook-pro.home>
2019-04-27 12:24:26 +0100

    Initial Commit

---
+++ TestXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestXcode
+//
+//  Created by Jiyoung on 27/04/2019.
+//  Copyright © 2019 Jiyoung Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
